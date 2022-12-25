5902b46cb29c8fa0e0f2492127e5ae58e15ef60e
doomout <winquake3@hanmail.net>
2022-12-26 01:10:43 +0900

    아웃렛 변수, 액션 함수 추가

--- Table/AddViewController.swift
+++ Table/AddViewController.swift
@@ -1,29 +1,16 @@
-//
-//  AddViewController.swift
-//  Table
-//
-//  Created by 현구김 on 2022/12/26.
-//
-
+import UIKit
 import UIKit
 
 class AddViewController: UIViewController {
 
+    @IBOutlet var tfAddItem: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnAddItem(_ sender: UIButton) {
     }
-    */
-
+    
 }
